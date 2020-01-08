83a4aeb13c97bd4e08e3bb363b852c99be6266d0
Joey <joseph.m.percival@gmail.com>
2020-01-08 05:59:50 +0000

    form validation

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,40 +1,77 @@
+/* eslint-disable no-undef */
+
 <template>
   <section class="page-section" id="contact">
     <div class="container">
       <div class="row">
         <div class="col-lg-12 text-center">
           <h2 class="section-heading text-uppercase">Send me a message</h2>
-          <h3 class="section-subheading text-muted">Please fill in the form below with your contact details and a short message on what you would like and I will contact you at your convenience - Thank you</h3>
+          <h3
+            class="section-subheading text-muted"
+          >Please fill in the form below with your contact details and a short message on what you would like and I will contact you at your convenience - Thank you</h3>
         </div>
       </div>
       <div class="row">
         <div class="col-lg-12">
-          <form id="contactForm" name="sentMessage" novalidate="novalidate">
+          <form id="contact">
             <div class="row">
               <div class="col-md-6">
                 <div class="form-group">
-                  <input class="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name." :rules="inputRules">
+                  <input
+                    class="form-control"
+                    id="name"
+                    type="text"
+                    placeholder="Your Name *"
+                    required="required"
+                    data-validation-required-message="Please enter your name."
+               
+                  />
                   <p class="help-block text-danger"></p>
                 </div>
                 <div class="form-group">
-                  <input class="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address.">
+                  <input
+                    class="form-control"
+                    id="email"
+                    type="email"
+                    placeholder="Your Email *"
+                    required="required"
+                    data-validation-required-message="Please enter your email address."
+                  />
                   <p class="help-block text-danger"></p>
                 </div>
                 <div class="form-group">
-                  <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number.">
+                  <input
+                    class="form-control"
+                    id="phone"
+                    type="tel"
+                    placeholder="Your Phone *"
+                    required="required"
+                    data-validation-required-message="Please enter your phone number."
+                  />
                   <p class="help-block text-danger"></p>
                 </div>
               </div>
               <div class="col-md-6">
                 <div class="form-group">
-                  <textarea class="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                  <textarea
+                    class="form-control"
+                    id="message"
+                    placeholder="Your Message *"
+                    required="required"
+                    data-validation-required-message="Please enter a message."
+                  ></textarea>
                   <p class="help-block text-danger"></p>
                 </div>
               </div>
               <div class="clearfix"></div>
               <div class="col-lg-12 text-center">
                 <div id="success"></div>
-                <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
+                <button
+                  
+                  class="btn btn-primary btn-xl text-uppercase"
+                  type="submit"
+                 v-on:click="say('what')"
+                >Send Message</button>
               </div>
             </div>
           </form>
@@ -42,26 +79,30 @@
       </div>
     </div>
   </section>
-
 </template>
 
 <script>
- const order = new Vue({
-   el: "#contactForm",
-   
-   data: {
-     form: {
-       name:null,
-       email: null,
-       phone: null,
-       message: null
-     }
-   },
+// eslint-disable-next-line no-undef
+new Vue({
+  el: "#contact",
 
-   mounted(){
-     alert('yes');
-   }
- });
+  data: {
+    form: {
+      name: null,
+      email: null,
+      phone: null,
+      message: null
+    }
+  },
+
+  methods: {
+    say: function (msg) {
+      alert(msg)
+    }
+  },
+
+  mounted() {}
+});
 
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-window.Vue = Vue;
 import App from './App.vue'
 import './assets/css/style.css'
 import './assets/css/bootstrap.css'
